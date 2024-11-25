c2d4235d8ec1e97ddfb3a4f7733a667b6284ae5b
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-11-25 18:49:42 +0530

    mod: updated constants

--- src/constant.js
+++ src/constant.js
@@ -2,4 +2,7 @@ export const CONTRACT_ADDRESS = "0x5825D8Aeb7A46B9DE858dF8aaE7a7898dA97b61e";
 
 export const RIDE_STATUS = ["Created", "Accepted", "Completed", "Cancelled"];
 
+export const PROGRAM_ID = "PEWDLGVG66cwYwjwppa4dtUXLmvwtqu5GyJrHKwcgEz";
+export const NETWORK = "https://api.devnet.solana.com";
+
 // https://gist.github.com/Abbas-Dev-786/09b988c88db02094a7d7ae4b628fcb69
