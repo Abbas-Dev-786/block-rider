0eb61b717f6a69fc400abddde4404424045e8751
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-11-25 11:23:49 +0530

    feat: created wallet context provider

---
+++ src/context/WalletProvider.jsx
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const WalletContextProvider = ({ children }) => {
+  const network = clusterApiUrl("devnet"); // You can change this to 'mainnet-beta' or 'testnet'
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={network}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AppKitProvider from "./context/AppKitProvider.jsx";
+import WalletContextProvider from "./context/WalletProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppKitProvider>
-        <App />
-      </AppKitProvider>
+      <WalletContextProvider>
+        <AppKitProvider>
+          <App />
+        </AppKitProvider>
+      </WalletContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
